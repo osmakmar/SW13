ea6e26e296c17ae6880bbed2aaf7d36a29c49450
Mario Osmakcic <osmakmar@students.zhaw.ch>
2023-06-03 18:49:01 +0200

    change

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 'use strict';
+
 const express = require('express');
+
 // Constants
 const PORT = 8081;
 const HOST = '0.0.0.0';
+
 // App
 const app = express();
 app.get('/', (req, res) => {
-res.send('Hello DevOps Course :-)');
+    res.send('Hello DevOps Course :-)');
 });
+
 app.listen(PORT, HOST);
-console.log(`Running on
-http://${HOST}:${PORT}`);
+console.log(`Running on http://${HOST}:${PORT}`);
